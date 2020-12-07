78383da66862e78039c24151072907dfea675b49
Reverse <59625582+codedipper@users.noreply.github.com>
2020-12-07 18:14:13 -0500

    Convert indentation to tabs.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const { chars } = require("./chars.json");
 
 function generate(num = 10, characters = chars){
-    if (num == "0") num = 10;
-    if (!Array.isArray(characters)) characters = chars;
+	if (num == "0") num = 10;
+	if (!Array.isArray(characters)) characters = chars;
 
-    let code = "";
+	let code = "";
 
-    for (let i = 0; i < parseInt(num); i++) {
-        code = code + characters[Math.floor(Math.random() * characters.length)];
-    }
+	for (let i = 0; i < parseInt(num); i++) {
+		code = code + characters[Math.floor(Math.random() * characters.length)];
+	}
 
-    return code;
-}
+	return code;
+};
 
 module.exports = generate;
