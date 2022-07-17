098e2f219cd97c1e47a0b2df081374bbbb0501f1
andgyk <an4g7k@gmail.com>
2022-07-17 18:59:15 +0600

    feat: modify parameter and add pattern option

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-";
 
-function gen(num = 10, chars = characters){
+function gen(num = 10, options = {}){
+	options = Object.assign({
+		chars: characters,
+		pattern: null
+	}, options);
+	const { chars, pattern } = options;
+
 	let code = "";
+	if (pattern) code = String(pattern);
 
 	if (num == "0" || num == null) num = 10;
 	for (let i = 0; i < parseInt(num); i++){
-		code += chars.split("")[Math.floor(Math.random() * chars.length)];
+		if (pattern) {
+			code = code.replace("#", __ranchar(chars));
+		} else {
+			code += __ranchar(chars);
+		}
 	}
 
 	return code;
+};
+
+function __ranchar (chars = characters) {
+	return chars.split("")[Math.floor(Math.random() * chars.length)];
 };
 
 module.exports = gen;
